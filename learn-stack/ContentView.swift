c05fa92f36b8edc9bc291c0355333d84ec9859d2
Stevhen <stevhen.work@gmail.com>
2021-03-10 01:29:30 +0700

    first commit

---
+++ learn-stack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learn-stack
+//
+//  Created by Stevhen on 09/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ my-widget/learn-stack-widget.swift
@@ -0,0 +1,42 @@
+//
+//  learn-stack-widget.swift
+//  learn-stack
+//
+//  Created by Stevhen on 09/03/21.
+//
+
+import SwiftUI
+import WidgetKit
+
+struct BellWidgetSmall: View {
+    var body: some View {
+        
+        ZStack {
+                
+            //let's code here
+            Image("bg")
+                .resizable()
+            
+            Image("circle-1")
+                .resizable()
+                .scaleEffect(0.7)
+            
+            Image("circle-2")
+                .resizable()
+                .scaleEffect(0.6)
+            
+            Image("bell")
+                .resizable()
+                .scaleEffect(0.4)
+            
+        }
+        
+    }
+}
+
+struct BellWidgetSmall_Previews: PreviewProvider {
+    static var previews: some View {
+        BellWidgetSmall()
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
